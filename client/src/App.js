c72281f6b14525e86050d9bd338ef652cc371731
rbalajee <rahrkb4@gmail.com>
2025-06-03 23:36:30 +0530

    dev changes

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React V4 GKE
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
